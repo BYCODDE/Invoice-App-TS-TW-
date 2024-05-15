e2654b67c937f27c4e52ac05edaac9c4d3b28529
marriamdd <davitashvili.m3@gmail.com>
2024-05-16 00:46:46 +0400

    feat:create context

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,56 @@ import "./App.css";
 import AppLayout from "./AppLayout";
 import Invoices from "./pages/Invoices";
 import InvoiceDetails from "./pages/InvoiceDetails";
-import { useState } from "react";
+import { createContext, useState } from "react";
 import data from "../data.json";
+
+interface IInvoices{
+  id: string;
+    createdAt: string;
+    paymentDue: string;
+    description: string;
+    paymentTerms: number;
+    clientName: string;
+    clientEmail: string;
+    status: string;
+    senderAddress: {
+        street: string;
+        city: string;
+        postCode: string;
+        country: string;
+    };
+    clientAddress: {
+      street:string,
+      city: string,
+      postCode: string,
+      country: string
+    };
+    items: {
+      name: string,
+      quantity: number,
+      price: number,
+      total: number
+        
+    }[];
+    total: number;
+}[]
+
+export const CategoryContext = createContext<{
+  isDarkMode:boolean;
+  setIsDarkMode:React.Dispatch<React.SetStateAction<boolean>>;
+  invoices:IInvoices[];
+  setInvoices:React.Dispatch<React.SetStateAction<IInvoices[]>>;
+
+}>({
+  isDarkMode: false,
+  setIsDarkMode: () => {},
+  invoices: [],
+  setInvoices: () => [],
+
+ 
+});
+
+ 
 
 const router = createBrowserRouter([
   {
@@ -24,9 +72,13 @@ const router = createBrowserRouter([
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [invoices, setInvoices] = useState(data);
+  const [invoices, setInvoices] = useState<IInvoices[]>(data);
 
-  return <RouterProvider router={router} />;
+  return (
+    <CategoryContext.Provider value={{ isDarkMode, setIsDarkMode, invoices, setInvoices }}>
+      <RouterProvider router={router} />
+    </CategoryContext.Provider>
+  );
 }
 
 export default App;
