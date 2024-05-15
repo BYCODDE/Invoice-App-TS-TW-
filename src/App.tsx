f821a3dbde708b41f6554278dcdbfb0de5083882
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-15 20:13:37 +0200

    The browser router was installed and the primary structure was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([{}]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
