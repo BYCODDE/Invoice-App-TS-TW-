5a1ded2f9f5614a35729e731cac9656368a4eae2
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-15 20:36:53 +0200

    registered pages in the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./AppLayout";
+import Invoices from "./pages/Invoices";
+import InvoiceDetails from "./pages/InvoiceDetails";
 
-const router = createBrowserRouter([{}]);
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        element: <Invoices />,
+        path: "/",
+      },
+      {
+        element: <InvoiceDetails />,
+        path: "/:id/invoiceDetails",
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/AppLayout.tsx
@@ -0,0 +1,5 @@
+// in this component the header should be first and then the outlet (what route are we on?)
+
+export default function AppLayout() {
+  return <div></div>;
+}
