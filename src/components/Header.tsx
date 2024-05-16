bc3ffeb5d0188814a7d10b0ffebbe75cbe8cb7bb
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-16 12:07:49 +0200

    styled the header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,12 @@
+import logo from "/assets/logo.svg";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="w-[375px] h-[72px] bg-[#373B53]">
+      <div className="w-[72px] h-full bg-[#7C5DFA] flex justify-center items-center rounded-r-[20px]">
+        <img src={logo} alt="logo" />
+      </div>
+      <div></div>
+    </div>
+  );
 }
