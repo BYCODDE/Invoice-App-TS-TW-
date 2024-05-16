ff2939f230ef764f81498a0a018c2d3f8a95c2a7
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-16 12:53:25 +0200

    change context name and make dinamic dark classname

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { InvoiceContext } from "./App";
+import { useContext } from "react";
 
 export default function AppLayout() {
+  const { isDarkMode } = useContext(InvoiceContext);
   return (
-    <>
+    <div className={isDarkMode ? "dark" : ""}>
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,30 @@
 import logo from "/assets/logo.svg";
+import moon from "/assets/icon-moon.svg";
+import avatar from "/assets/image-avatar.jpg";
+import { useContext } from "react";
+import { InvoiceContext } from "../App";
 
 export default function Header() {
+  const { setIsDarkMode } = useContext(InvoiceContext);
+
   return (
-    <div className="w-[375px] h-[72px] bg-[#373B53]">
+    <div className="flex justify-between pr-[24px] items-center w-[375px] h-[72px] bg-[#373B53]">
       <div className="w-[72px] h-full bg-[#7C5DFA] flex justify-center items-center rounded-r-[20px]">
         <img src={logo} alt="logo" />
       </div>
-      <div></div>
+      <div className="flex items-center">
+        <img
+          src={moon}
+          alt="moon"
+          className="w-[20px] h-[20px]"
+          onClick={() => setIsDarkMode((dark) => !dark)}
+        />
+        <img
+          src={avatar}
+          alt="avatar"
+          className="w-[32px] h-[32px] rounded-full"
+        />
+      </div>
     </div>
   );
 }
