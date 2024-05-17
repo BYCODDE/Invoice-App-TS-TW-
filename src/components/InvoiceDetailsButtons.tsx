8d0bac99524d86945e46a84914881dcc8d128f82
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-17 17:50:02 +0200

    combined the status and button components for the tablet

--- src/components/InvoiceDetailsButtons.tsx
+++ src/components/InvoiceDetailsButtons.tsx
@@ -1,6 +1,6 @@
 export default function InvoiceDetailsButtons() {
   return (
-    <div className="pt-[21px] px-[24px] pb-[22px] bg-[#FFFFFF] dark:bg-[#1E2139] flex items-center gap-[8px] mt-[56px]">
+    <div className=" pt-[21px] px-[24px] pb-[22px] bg-[#FFFFFF] dark:bg-[#1E2139] flex items-center justify-center gap-[8px] mt-[56px] md:p-0 md:m-0 ">
       <button className="pt-[18px] px-[24px] pb-[15px] bg-[#F9FAFE] dark:bg-[#252945] text-[#7E88C3] dark:text-[#DFE3FA] rounded-[24px] text-[15px] font-bold leading-[15px] tracking-[-0.25px]">
         Edit
       </button>
--- src/components/InvoiceStatusInfo.tsx
+++ src/components/InvoiceStatusInfo.tsx
@@ -1,14 +1,21 @@
+import InvoiceDetailsButtons from "./InvoiceDetailsButtons";
+
 export default function InvoiceStatusInfo() {
   return (
-    <div className="bg-[#FFFFFF] dark:bg-[#1E2139] pt-[24px] pb-[27px] px-[24px] mt-[31px] flex justify-between items-center rounded-[8px]">
-      <p className="text-[#858BB2] dark:text-[#DFE3FA] font-medium text-[13px] leading-[15px] tracking-[-0.1px]">
-        Status
-      </p>
-      <div className="flex gap-[8px] items-center pt-[14px] pr-[19px] pb-[11px] pl-[18px] bg-[#ff910011] rounded-[5px]">
-        <div className="w-[8px] h-[8px] rounded-full bg-[#FF8F00] mb-[2px]"></div>
-        <p className="text-[#FF8F00] font-bold text-[15px] leading-[15px] tracking-[-0.25px]">
-          Pending
+    <div className="md:flex md:justify-between md:bg-[#FFFFFF] md:dark:bg-[#1E2139] md:mt-[31px] md:py-[20px] md:px-[32px]">
+      <div className="bg-[#FFFFFF] dark:bg-[#1E2139] pt-[24px] pb-[27px] px-[24px] mt-[31px] flex justify-between items-center rounded-[8px] md:justify-start md:p-0 md:m-0 md:gap-[20px]">
+        <p className="text-[#858BB2] dark:text-[#DFE3FA] font-medium text-[13px] leading-[15px] tracking-[-0.1px]">
+          Status
         </p>
+        <div className="flex gap-[8px] items-center pt-[14px] pr-[19px] pb-[11px] pl-[18px] bg-[#ff9100] bg-opacity-[0.0571] rounded-[6px]">
+          <div className="w-[8px] h-[8px] rounded-full bg-[#FF8F00] mb-[2px]"></div>
+          <p className="text-[#FF8F00] font-bold text-[15px] leading-[15px] tracking-[-0.25px]">
+            Pending
+          </p>
+        </div>
+      </div>
+      <div className="hidden md:block">
+        <InvoiceDetailsButtons />
       </div>
     </div>
   );
