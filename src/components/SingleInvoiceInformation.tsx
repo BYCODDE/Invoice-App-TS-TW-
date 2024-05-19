91a0821f312fc12fd24818569c36e0f04cb7980c
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-05-19 21:58:24 +0200

    Complete design including Responsiveness and Dark Mode

--- src/components/SingleInvoiceInformation.tsx
+++ src/components/SingleInvoiceInformation.tsx
@@ -3,7 +3,7 @@ import InvoicePaymentInfoLarger from "./InvoicePaymentInfoLarger";
 
 export default function SingleInvoiceInformation() {
   return (
-    <div className="mt-[16px] pt-[25px] px-[24px] pb-[24px] bg-[#FFF] dark:bg-[#1E2139] rounded-[8px] md:p-[32px]">
+    <div className="mt-[16px] pt-[25px] px-[24px] pb-[24px] bg-[#FFF] dark:bg-[#1E2139] rounded-[8px] md:p-[32px] xl:pt-[50px] xl:px-[48px] xl:pb-[48px]">
       <div className="md:flex md:justify-between">
         <div>
           <p className="text-[#7E88C3] dark:text-[#858BB2] font-bold text-[15px] leading-[15px] tracking-[-0.25px]">
@@ -71,7 +71,7 @@ export default function SingleInvoiceInformation() {
           </div>
         </div>
 
-        <div className="mt-[32px] flex flex-col gap-[13px]">
+        <div className="mt-[32px] flex flex-col gap-[13px] md:mt-0">
           <p className="text-[#7E88C3] dark:text-[#DFE3FA] font-medium text-[13px] leading-[18px] tracking-[-0.1px]">
             Sent to
           </p>
